1c8de41129e46ea4716b52d3bc13fc58ba082167
YangFan <imqqyangfan@gmail.com>
2020-11-09 23:08:06 +0800

    init lfs

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+﻿/*
+    2020.11.9 by yangfan
+*/
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.hpp>
+#include <glm/glm.hpp>
+
+int main() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported" << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
+}
